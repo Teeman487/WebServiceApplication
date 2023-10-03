c1aae03288d9849811573b8114a1a50685cc0769
Akinade Toheeb Adebusoye <adebusoyeteeman@gmail.com>
2023-10-03 11:00:59 +0100

    T

---
+++ src/main/java/com/wsapp/wsapplication/WsapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.wsapp.wsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsapp/wsapplication/WsapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsapp.wsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
